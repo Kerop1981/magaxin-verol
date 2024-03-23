48ed6075dbdf15e3e9fbdd23213ed94561eaced7
Kerop1981 <vova.abraamyan@mail.ru>
2024-03-23 15:39:37 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { SceltaComponent } from './scelta/scelta.component';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar';
+import {CascadeSelectModule} from 'primeng/cascadeselect';
+import { BasicComponent } from './scelta/basic/basic.component';
+import { TemplatingComponent } from './scelta/basic/templating/templating.component';
+import { KalendarComponent } from './scelta/kalendar/kalendar.component';
+import {CheckboxModule} from 'primeng/checkbox';
+import { PasangerComponent } from './scelta/pasanger/pasanger.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SceltaComponent,
+    BasicComponent,
+    TemplatingComponent,
+    KalendarComponent,
+    PasangerComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    InputTextModule,
+    CascadeSelectModule,
+    CalendarModule,
+    CheckboxModule,
+   
   ],
+  exports: [SceltaComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
